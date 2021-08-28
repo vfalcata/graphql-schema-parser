bd46fe65a4f58280fb61f4d41dc5053dc314da61
vfalcata <vfalcata@>
2021-08-27 23:38:26 -0400

    added file reader temporarily to test/debug, will be removed once completed

--- src/graphql-schema-parser.js
+++ src/graphql-schema-parser.js
@@ -17,26 +17,26 @@ const generateSchemaObject = (graphqlSchemaTextString) => {
     const __dirname = dirname(__filename);
     const file = readFileSync(resolve(__dirname,'./__test__/fixtures/testschema.graphql'), 'utf8');
     console.log(file)
-    // const { encodedDirectivesSchemaText, directiveProperties } = parseDirectives(graphqlSchemaTextString)
-    // console.log('enco',directiveProperties)
-    // const types = getTypes(encodedDirectivesSchemaText,directiveProperties)
-    // console.log('types',types)
-    // // // console.log('typessss',types.type.Query_isExtended_.directives.include1.parameters)
+    const { encodedDirectivesSchemaText, directiveProperties } = parseDirectives(graphqlSchemaTextString)
+    console.log('enco',directiveProperties)
+    const types = getTypes(encodedDirectivesSchemaText,directiveProperties)
+    console.log('types',types)
+    // // console.log('typessss',types.type.Query_isExtended_.directives.include1.parameters)
 
-    // const unions = getUnions(encodedDirectivesSchemaText,directiveProperties)
-    // console.log('unions',unions)
+    const unions = getUnions(encodedDirectivesSchemaText,directiveProperties)
+    console.log('unions',unions)
 
-    // const scalars = getScalars(encodedDirectivesSchemaText,directiveProperties)
-    // console.log('scalars',scalars)
+    const scalars = getScalars(encodedDirectivesSchemaText,directiveProperties)
+    console.log('scalars',scalars)
 
-    // const enums = getEnums(encodedDirectivesSchemaText,directiveProperties)
-    // console.log('enums',enums)
+    const enums = getEnums(encodedDirectivesSchemaText,directiveProperties)
+    console.log('enums',enums)
 
-    // const enums = getEnums(encodedDirectivesSchemaText,directiveProperties)
-    // console.log('enums',enums)
+    const enums = getEnums(encodedDirectivesSchemaText,directiveProperties)
+    console.log('enums',enums)
 
-    // const scalars = getScalars(encodedDirectivesSchemaText,directiveProperties)
-    // console.log('enums',scalars)
+    const scalars = getScalars(encodedDirectivesSchemaText,directiveProperties)
+    console.log('enums',scalars)
 
 }
 //returns graphql schema object, as per typedef
